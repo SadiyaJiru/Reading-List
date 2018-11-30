10ca92856229fef045ffdb5375576e1310eff144
sadiyaj <sadiyajiru@gmail.com>
2018-11-29 21:06:27 -0600

    Heroku issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/books";
-import Nav from "./components/Nav";
+import Nav from "./components/Nav/index";
 import MoreInfo from "./pages/moreInfo";
 import NoMatch from "./pages/NoMatch";
 
