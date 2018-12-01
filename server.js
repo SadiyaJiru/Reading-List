45e0f11fa61277f383ea7aa74d170e4fc2c3cd70
sadiyaj <sadiyajiru@gmail.com>
2018-12-01 14:14:49 -0600

    Trying to push to heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
